b342002d1f641f8f67413f937b68489a2cd6cd6c
Eyal Toledano <eyaltoledano@me.com>
2019-03-23 02:29:08 +0000

    Done.

--- lib/artist.rb
+++ lib/artist.rb
@@ -18,13 +18,8 @@ class Artist
   end
 
   def new_song(name, genre)
-    song = Song.new(name, genre)
-    songs << song
+    song = Song.new(name, self, genre)
     # binding.pry
-  end
-
-  def songs
-    @songs
   end
 
 end
--- lib/song.rb
+++ lib/song.rb
@@ -1,7 +1,7 @@
 class Song
   attr_accessor :name, :genre, :artist
 
-  def initialize(name, genre)
+  def initialize(name, genre, artist = nil)
     @name = name
     @genre = genre
   end
